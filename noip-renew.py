7036a781dc7d745659805f9a21ef444006988249
YFHD <ethan941106@gmail.com>
2024-05-26 13:52:54 +0800

    Update noip-renew.py

--- noip-renew.py
+++ noip-renew.py
@@ -56,7 +56,7 @@ class Robot:
     latest_version = requests.get("https://chromedriver.storage.googleapis.com/LATEST_RELEASE_114")
     path = ChromeDriverManager(driver_version=latest_version.text).install()
     options.binary_location = path
-    self.browser = webdriver.Chrome(options=options)
+    self.browser = webdriver.Chrome(options=options, service=ChromeService(path))
     self.browser.set_page_load_timeout(90) # Extended timeout for Raspberry Pi.
 
   def login(self):
